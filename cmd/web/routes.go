8150b54ca9357f34b857c2a3598e524166297d24
m54h <46087557+m54h@user.noreply.github.com>
2023-12-06 10:02:04 +0800

    add and use httprouter library

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,12 +11,12 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handle("http.MethodGet", "/static/*filepath", http.StripPrefix("/static", fileServer))
+	router.Handler("http.MethodGet", "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	router.HandleFunc(http.MethodGet, "/", app.home)
-	router.HandleFunc(http.MethodGet, "/snippet/view", app.snippetView)
-	router.HandleFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
-	router.HandleFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	return standard.Then(router)
