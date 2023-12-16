f08d6a6afb5766f945190a5347fd3fe8fa41ada8
m54h <46087557+m54h@user.noreply.github.com>
2023-12-16 13:24:45 +0800

    redirect unauthenticated user from snippet/create to login

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -5,6 +5,18 @@ import (
 	"net/http"
 )
 
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		w.Header().Add("Cache-control", "no-store")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
